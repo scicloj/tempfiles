131143754f749ca71ff8f31548946afd1c5cf496
daslu <daniel.slutsky@gmail.com>
2021-10-08 23:51:47 +0300

    corrected version to SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.scicloj/tempfiles "1-alpha2"
+(defproject org.scicloj/tempfiles "1-alpha2-SNAPSHOT"
   :description "A small library for handling temporary files to be served by a web server."
   :url "https://github.com/scicloj/tempfiles"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
