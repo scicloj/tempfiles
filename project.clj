894d1b57d612ae8d491e063bb37866c2ed028da4
daslu <daniel.slutsky@gmail.com>
2024-01-29 07:58:13 +0200

    version "1-alpha3"; new build setup

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject org.scicloj/tempfiles "1-alpha2"
-  :description "A small library for handling temporary files to be served by a web server."
-  :url "https://github.com/scicloj/tempfiles"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [babashka/fs "0.0.1"]])
